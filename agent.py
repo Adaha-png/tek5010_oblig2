b59e96878d3f7c4deace00c356aa10ef8e55c5f0
adaha-png <ada.hatland@gmail.com>
2024-10-22 14:58:03 +0200

    first commit

---
+++ agent.py
@@ -0,0 +1,117 @@
+import random
+
+import numpy as np
+
+
+class Agent:
+    def __init__(self, speed=25, search_range=250):
+        self.pos = np.random.rand(2) * 1000
+        self.v = speed
+        self.wait = 0
+        self.w_max = 100
+        self.c = 1
+        self.di = np.random.rand() * 2 * np.pi
+        self.search_range = search_range
+
+    def move(self, tasks, agents):
+        if self.wait <= 0:
+            self.pos[0] += self.v * np.cos(self.di)
+            self.pos[1] += self.v * np.sin(self.di)
+
+            if not (0 < self.pos[0] < 1000) or not (0 < self.pos[1] < 1000):
+                self.pos = np.clip(self.pos, a_min=0, a_max=1000)
+                self.di = np.random.rand() * 2 * np.pi
+
+            for t in tasks:
+                if np.linalg.norm(self.pos - t.pos) <= t.r:
+                    self.wait = max(
+                        np.round(
+                            (self.w_max * self.temperature(agents) ** 2)
+                            / (self.temperature(agents) ** 2 + self.c)
+                        ),
+                        1,
+                    )
+                    self.di = (self.di + np.pi) % (2 * np.pi)
+                    self.callout(agents)
+
+                    break
+
+        else:
+            self.wait -= 1
+
+    def move_with_auction(self, tasks, agents):
+        if self.wait <= 0:
+            self.pos[0] += self.v * np.cos(self.di)
+            self.pos[1] += self.v * np.sin(self.di)
+
+            if not (0 < self.pos[0] < 1000) or not (0 < self.pos[1] < 1000):
+                self.pos = np.clip(self.pos, a_min=0, a_max=1000)
+                self.di = np.random.rand() * 2 * np.pi
+
+            for t in tasks:
+                if np.linalg.norm(self.pos - t.pos) <= t.r:
+                    self.wait = max(
+                        np.round(
+                            (self.w_max * self.temperature(agents) ** 2)
+                            / (self.temperature(agents) ** 2 + self.c)
+                        ),
+                        1,
+                    )
+                    self.di = (self.di + np.pi) % (2 * np.pi)
+                    self.auction(agents, t.capacity)
+
+                    break
+
+        else:
+            self.wait -= 1
+
+    def temperature(self, agents):
+        return len(
+            [a for a in agents if np.linalg.norm(a.pos - self.pos) <= self.search_range]
+        )
+
+    def callout(self, agents):
+        for a in agents:
+            if np.linalg.norm(a.pos - self.pos) < self.search_range and a is not self:
+                a.call(self)
+
+    def auction(self, agents, n_agents):
+        # Calculate the distance of each agent from self
+        distances = [
+            (a, np.linalg.norm(a.pos - self.pos)) for a in agents if a is not self
+        ]
+
+        # Sort agents by distance
+        sorted_agents = sorted(distances, key=lambda x: x[1])
+
+        closest_agents = sorted_agents[:n_agents]
+
+        # Call the closest agents
+        for agent, distance in closest_agents:
+            if distance < self.search_range:
+                agent.call(self)
+
+    def call(self, agent):
+        direction_vector = agent.pos - self.pos
+        self.di = np.arctan2(direction_vector[1], direction_vector[0])
+
+
+class Task:
+    def __init__(self, capacity=1, radius=50):
+        self.capacity = capacity
+        self.pos = np.random.rand(2) * 1000
+        self.r = radius
+
+    def solve(self, agents):
+        actors = []
+
+        for a in agents:
+            if np.linalg.norm(a.pos - self.pos) <= self.r:
+                actors.append(a)
+                if len(actors) >= self.capacity:
+                    self.pos = np.random.rand(2) * 1000
+                    for act in actors:
+                        act.wait = 0
+                    return True
+
+        return False
---
+++ main.py
@@ -0,0 +1,66 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
+
+from agent import Agent, Task
+
+
+def simulate(agents, tasks, maxitr=100000):
+    solved = 0
+    moving = np.zeros(maxitr)
+    for i in tqdm(range(maxitr)):
+        for agent in agents:
+            agent.move(tasks, agents)
+            moving[i] += agent.wait == 0
+
+        for task in tasks:
+            solved += task.solve(agents)
+
+    return solved / maxitr, moving / len(agents)
+
+
+if __name__ == "__main__":
+    # capacities = [1, 3]
+    # num_agents = [1, 3, 5, 10, 20, 30]
+    # for c in capacities:
+    #     solved = []
+    #     for num in num_agents:
+    #         agents = [Agent(search_range=0) for _ in range(num)]
+    #         tasks = [Task(capacity=c) for _ in range(1)]
+    #         solv, mov = simulate(agents, tasks)
+    #         solved.append(solv)
+    #         plt.plot(mov)
+    #         plt.show()
+    #
+    #     plt.figure(figsize=(10, 6))
+    #     bar_width = 0.8  # Width of the bars
+    #     positions = range(len(num_agents))  # X-axis positions for the bars
+    #     plt.bar(positions, solved, width=bar_width, color="blue")
+    #     plt.xlabel("Number of Agents")
+    #     plt.ylabel("Tasks Solved per iteration")
+    #     plt.title("BEECLUSTesque solving tasks")
+    #     plt.xticks(positions, num_agents)  # Set the positions and labels for x-ticks
+    #     plt.grid(axis="y")
+    #     plt.savefig(f"{c}_capacity.png")
+
+    capacity = 3
+    num_agents = 30
+    search_ranges = [0, 100, 200, 300, 400, 600, 1000, 1400]
+    num_tasks = 2
+    solved = []
+    for sr in search_ranges:
+        agents = [Agent(search_range=sr) for _ in range(num_agents)]
+        tasks = [Task(capacity=capacity) for _ in range(num_tasks)]
+        solv, mov = simulate(agents, tasks)
+        solved.append(solv)
+
+    plt.figure(figsize=(10, 6))
+    bar_width = 0.8  # Width of the bars
+    positions = range(len(search_ranges))  # X-axis positions for the bars
+    plt.bar(positions, solved, width=bar_width, color="blue")
+    plt.xlabel("Communication distance")
+    plt.ylabel("Tasks Solved per iteration")
+    plt.title(f"Solving tasks with call-out")
+    plt.xticks(positions, search_ranges)  # Set the positions and labels for x-ticks
+    plt.grid(axis="y")
+    plt.savefig(f"search_ranges.png")
